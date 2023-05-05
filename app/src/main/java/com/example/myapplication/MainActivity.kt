f8e212b364ff68877f859550336e73a49ca87c0a
renji <renjith59@gmail.com>
2023-05-05 19:23:16 +0530

    Log in MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Tag","Test Android git")
     }
     fun getApplicationTeam(): Int {
         return 0
